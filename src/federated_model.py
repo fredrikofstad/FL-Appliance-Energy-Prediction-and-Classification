47e80b3c54220e87fdbc61c077170b3539561d31
fredrikofstad <nokotin@gmail.com>
2023-11-11 17:37:13 +0100

    trying to get loss

--- src/federated_model.py
+++ src/federated_model.py
@@ -80,8 +80,8 @@ def train(network="RNN"):
         result = training_process.next(train_state, federated_train_data)
         train_state = result.state
         train_metrics = result.metrics
-        loss_values.append(train_metrics['loss'])
-        print('round {:2d}, metrics={}'.format(round_num, train_metrics))
+        #loss_values.append(train_metrics['loss'])
+        print(f"Metrics for round {round_num}: {train_metrics}")
 
     return loss_values
 
